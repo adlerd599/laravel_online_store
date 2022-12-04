8fa70a3baeb41912971e8ad33a3e2d6ee1e8bfee
D <aslers599@gmail.com>
2022-12-04 19:10:51 +0200

    bootstrap installation

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/js/app.js'])
+    
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="my_class">12345</h1>
+    <div class="container">
+        <h2>Button Sizes</h2>
+        <button type="button" class="btn btn-primary btn-lg">Large</button>
+        <button type="button" class="btn btn-primary">Normal</button>    
+        <button type="button" class="btn btn-primary btn-sm">Small</button>
+        <button type="button" class="btn btn-primary btn-xs">XSmall</button>
+    </div>
+</body>
+</html>
